a2321a267533f953d7a0070af198c243c9c46c51
Luke Prosser <lukeprosser@Lukes-MBP.lan>
2023-10-28 12:49:28 +0100

    working contact form with email send

---
+++ components/contact-form/ContactForm.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+
+export default function ContactForm() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = {};
+    Array.from(e.currentTarget.elements).forEach((field) => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    });
+    // console.log('formData', formData);
+
+    await fetch('/api/mail', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+  };
+
+  return (
+    <form method="post" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          type="text"
+          name="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          name="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="emailAddress">Email Address</label>
+        <input
+          type="email"
+          name="emailAddress"
+          value={emailAddress}
+          onChange={(e) => setEmailAddress(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message</label>
+        <textarea
+          name="message"
+          rows={4}
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </div>
+      <button type="submit">Send</button>
+    </form>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import ContactForm from '../../../components/contact-form/ContactForm';
+
+export default function Contact() {
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-center text-2xl font-medium">Contact</h1>
+      <div>
+        <ContactForm />
+      </div>
+    </div>
+  );
+}
