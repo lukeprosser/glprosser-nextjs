4db618968e2f06d85a7103c834b10aea7b35628d
Luke Prosser <lukesprosser@gmail.com>
2024-01-20 21:23:42 +0000

    testimonial styles

--- src/sections/testimonials/Testimonials.tsx
+++ src/sections/testimonials/Testimonials.tsx
@@ -1,24 +1,45 @@
 import Container from '@/components/container/Container';
 
+import { ImQuotesLeft, ImQuotesRight } from 'react-icons/im';
+
 export default function Testimonials() {
   return (
     <section>
       <Container>
-        <div className="py-16 text-center font-light text-lg grid md:grid-cols-2 items-center gap-8 lg:gap-10">
-          <div>
-            <p className="mb-2 italic">
-              “Dear Gareth, thank you for the excellent work you have done for
-              me. I cannot praise you enough for the quality of your
-              craftsmanship. You did a great job.”
+        <div className="mx-auto max-w-screen-lg py-16 text-center font-light text-lg grid md:grid-cols-2 justify-items-center gap-10 lg:gap-12">
+          <div className="h-full max-w-sm flex flex-col justify-between border border-stone-300 shadow-lg p-10 bg-white">
+            <p className="relative mb-6 italic">
+              <ImQuotesLeft
+                size={30}
+                className="absolute -top-5 -left-7 text-stone-200"
+              />
+              Gareth, thank you for the excellent work you have done for me. I
+              cannot praise you enough for the quality of your craftsmanship.
+              <ImQuotesRight
+                size={30}
+                className="absolute -bottom-2 -right-5 text-stone-200"
+              />
             </p>
-            <p className="text-base">Paul Murphy, MP</p>
+            <p className="text-sm text-red-500 uppercase font-medium tracking-widest">
+              Paul Murphy, MP
+            </p>
           </div>
-          <div>
-            <p className="mb-2 italic">
-              “We are very pleased with the work that Gareth did on our
-              extension and highly recommend him!”
+          <div className="h-full max-w-sm flex flex-col justify-between border border-stone-300 shadow-lg p-10 bg-white">
+            <p className="relative mb-6 italic">
+              <ImQuotesLeft
+                size={30}
+                className="absolute -top-5 -left-7 text-stone-200"
+              />
+              We are very pleased with the work that Gareth did on our extension
+              and highly recommend him!
+              <ImQuotesRight
+                size={30}
+                className="absolute -bottom-2 -right-5 text-stone-200"
+              />
             </p>
-            <p className="text-base">Sue Holroyd</p>
+            <p className="text-sm text-red-500 uppercase font-medium tracking-widest">
+              Sue Holroyd
+            </p>
           </div>
         </div>
       </Container>
